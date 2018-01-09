cd3ddd4e110ac76af4e6cd1cda20ddae6d74756d
Ryan LaBouve <imryanlabouve@gmail.com>
2018-01-08 23:07:02 -0600

    Initial commit

---
+++ bot.js
@@ -0,0 +1,32 @@
+var GitHubApi = require('github'),
+  debug = require('debug')('reviewbot:bot'),
+  config = require('../config');
+
+var options = {version: '3.0.0'}
+if (config.github) {
+  options.host = config.github;
+  options.pathPrefix = "/api/v3";
+}
+var github = new GitHubApi(options);
+
+/**
+ * Private: Authenticate next request
+ */
+function _authenticate() {
+  if ((!config.botUser || !config.botPassword) && !config.oauth2token) {
+    throw Error('Fatal: No username/password or no Oauth2 token configured!');
+  }
+
+  if (config.oauth2token) {
+    github.authenticate({
+      type: 'oauth',
+      token: config.oauth2token
+    });
+  } else {
+    github.authenticate({
+      type: 'basic',
+      username: config.botUser,
+      password: config.botPassword
+    });
+  }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# diffed-bot
+
+## Development
+
+DEBUG=diffed-bot:* npm start
+
+## Install
+
+## Thank-you's
+
+* https://github.com/felixrieseberg/peer-review-bot
